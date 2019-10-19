559e362de05388828a354fd7a3b2569dcf367974
Erwin Pascual <56744638+pascuale2@users.noreply.github.com>
2019-10-19 16:37:38 -0600

    Yeeted New Files

--- GOCanada.py
+++ GOCanada.py
@@ -1,11 +1,32 @@
-from fileopencsv import *
+import Fileopencsv, Compute_data, statistics
 '''
 This is our main file
 '''
 
 def main():
-    input_file_list = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    input_file_list = file_interpret_csv("dataset1.csv")
-    print(input_file_list)
+    data_BY_COLUMN = Fileopencsv.get_column_csv("dataset1.csv")
 
+    MEAN_data_C = Compute_data.compute_mean(data_BY_COLUMN)
+    STDEV_data_C = Compute_data.compute_standard_deviation(data_BY_COLUMN)
+
+    data_wout_outliers_C = Compute_data.remove_outliers(data_BY_COLUMN, MEAN_data_C)
+    data_just_outliers_C = Compute_data.just_outliers(data_BY_COLUMN, MEAN_data_C)
+
+
+
+
+    data_BY_ROW = Fileopencsv.get_row_csv("dataset1.csv")
+
+    MEAN_data_R = Compute_data.compute_mean(data_BY_ROW)
+    STDEV_data_R = Compute_data.compute_standard_deviation(data_BY_ROW)
+
+    data_wout_outliers_R = Compute_data.remove_outliers(data_BY_ROW, MEAN_data_R)
+    data_just_outliers_R = Compute_data.just_outliers(data_BY_ROW, MEAN_data_R)
+    print(data_just_outliers_R)
+
+
+
+
+
+    
 main()
