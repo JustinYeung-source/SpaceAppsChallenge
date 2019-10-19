b0686109420754ce20f6959d3464a93ea3e9c983
Rozario B <r_beaudin@outlook.com>
2019-10-19 12:49:45 -0600

    added base
    I'm pushing this now because i need to trancribe code

---
+++ .vscode/GeoMagneticData.py
@@ -0,0 +1,12 @@
+import csv
+
+class GeoMagneticData():
+
+    def __init__(self, idNum):
+        self.test = 5
+        self.idNum = idNum
+
+
+    def __str__(self):
+        return self.idNum
+
---
+++ .vscode/Main.py
@@ -0,0 +1,7 @@
+import csv
+from GeoMagneticData import *
+
+def getFile(fileName):
+    file = csv.reader(fileName)
+
+    
