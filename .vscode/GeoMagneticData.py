7d1852cf2e118117f7f4e65db5a0862da782d253
Rozario B <r_beaudin@outlook.com>
2019-10-19 18:28:21 -0600

    added some comments
    read title

--- .vscode/GeoMagneticData.py
+++ .vscode/GeoMagneticData.py
@@ -2,10 +2,12 @@ import csv
 
 class GeoMagneticData():
 
+
     def __init__(self, location, longitude, latitude, dataIndex):
         self._location = location
         self._longtitude = longitude
         self._latitude = latitude
+        # Each index is a row I.E it is the DD/HH in the file
         self._data = []
         # IMPORTANT: this index relates to which column this classes data is
         self._dataIndex = dataIndex
