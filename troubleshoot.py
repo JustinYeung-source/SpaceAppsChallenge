8e83821b17c17461a5a984a4335791ff4fba0101
Tuan Lieu <38875039+tmlieu1@users.noreply.github.com>
2019-10-19 18:36:36 -0600

    modified days/hours code, refined others, commented a bit

--- troubleshoot.py
+++ troubleshoot.py
@@ -1,5 +1,6 @@
 import statistics
 from fileopencsv import *
+from GeoMagneticData import *
 '''
 iterate through the list.
 if the element is within the range, then it's accepted and we 
@@ -30,11 +31,22 @@ def anomalies_row(in_list):
             temp.append(i)
             temp.append(in_list[i])
             anomaly_list.append(temp)
+            
     return anomaly_list
+
+def findGeoDataIndex(theList, theIndex):
+    for item in theList:
+        if item.getDataIndex() == theIndex:
+            return item
+    
 
 def main():
     input_file_list = getrow_csv("dataset1.csv")
-    anom = anomalies_row(input_file_list[0])
-    #print(input_file_list[0])
-    print(anom)
+
+    for i in range (len(input_file_list)):
+        anom = anomalies_row(input_file_list[i])
+        print(i, day(i), hour(i), end = ":")
+        for j in range (len(anom)):
+            print(anom[j], end = " ")
+        print("")
 main()
